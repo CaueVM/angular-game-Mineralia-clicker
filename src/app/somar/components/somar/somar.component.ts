04e111e95d4dcffb9a69a915a82f5a5706196755
CaueVM <caue.mendes01@etec.sp.gov.br>
2019-10-18 16:59:49 -0300

    TA FUNFANDO

--- src/app/somar/components/somar/somar.component.ts
+++ src/app/somar/components/somar/somar.component.ts
@@ -12,27 +12,59 @@ export class SomarComponent implements OnInit {
   private quantiaMinerio: number;
   private quantiaOuro: number;
   private valorFerro: number;
-  private ferroMinerado: number;
+  private novaQuantiaOuro: number;
+  private valorMineracao: number;
 
   ngOnInit() {
     this.quantiaMinerio = 0;
-    this.quantiaOuro  = 0;
+    this.quantiaOuro  = 100;
 
   }
   
+  novaPicareta(power: number){
+    this.valorMineracao = power;
+  }
 
-  calculaMinerar(valorRecebido: number){
-    this.quantiaMinerio = this.somar.somar(this.quantiaMinerio,valorRecebido);
+  calculaMinerar(){
+    this.quantiaMinerio = this.somar.somar(this.quantiaMinerio, this.valorMineracao);
+  }
+
+  converteFerro(){
+    this.quantiaOuro = this.quantiaOuro + (this.quantiaMinerio / 4);
+    console.log(this.quantiaOuro);
+    this.reseta();
   }
 
   reseta(){
     this.quantiaMinerio = 0;
-    this.quantiaOuro = 0;
+  }
+
+  resetAll(){
+    this.valorMineracao = 1;
+    this.quantiaMinerio = 0;
+    this.quantiaOuro = 1000;
+  }
+
+  comprarPicareta1(preco: number, poder: number ){
+    var power = poder;
+    if(this.quantiaOuro >= preco){
+      console.log(poder);
+      this.quantiaOuro = this.quantiaOuro - preco;
+      this.novaPicareta(power);
+      return this.quantiaOuro;
+    }else{
+      alert("minera mais meu fi");
+    }
+  }
+
+  get pegaValorConvertidoOuro(){
+    return this.quantiaOuro;
   }
 
   get pegaQuantiaMinerio(){
     return this.quantiaMinerio;
   }
+
   get pegaQuantiaOuro(){
     return this.quantiaOuro;
   }
